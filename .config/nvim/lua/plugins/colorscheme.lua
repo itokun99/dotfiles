565241bac4dc6ba431f66fb9b3d6089ccfe26d44
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2024-02-13 12:12:57 +0700

    update config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
   -- add dracula
-  { "Mofiqul/dracula.nvim", opts = {
-    transparent_bg = true,
-    italic_comment = true,
-  } },
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      transparent_bg = true,
+      italic_comment = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 
   -- Configure LazyVim to load dracula
   -- {
