a4195d1f2aeaf803997bad3964bc3e86f96808c6
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2024-04-04 22:58:33 +0700

    update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,20 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+wezterm.on("update-right-status", function(window, pane)
+	-- "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M ")
+
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+
+	window:set_right_status(wezterm.format({
+		{ Text = bat .. "   " .. date },
+	}))
+end)
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
