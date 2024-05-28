4094dba303e30e3a3dd060109c25f1557b94828f
Indrawan Lisanto <indrawanlisantopersonal@gmail.com>
2024-05-28 09:03:57 +0700

    update

---
+++ .config/fish/config.fish
@@ -0,0 +1,54 @@
+## Android
+set -gx --export ANDROID_HOME $HOME/Library/Android/Sdk
+set -gx PATH $ANDROID_HOME/emulator $PATH
+set -gx PATH $ANDROID_HOME/tools $PATH
+set -gx PATH $ANDROID_HOME/tools/bin $PATH
+set -gx PATH $ANDROID_HOME/platform-tools $PATH
+set -gx PATH /Library/PostgreSQL/15/bin $PATH
+
+# Golang developers might need this one 
+set -xg GOPATH $HOME/gocode
+
+# Python developers otherwise
+set -xg PYTHONDONTWRITEBYTECODE 1
+set -xg fish_tmux_autostart false
+set -xg fish_tmux_autostart_once false
+set -xg fish_tmux_autoconnect true
+set -xg fish_tmux_autostarted false
+set -Ux fish_tmux_config $HOME/.config/tmux.conf
+
+# JAVA Variables
+set -xg JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-11.jdk/Contents/Home
+set -xg GEM_HOME $HOME/.gem
+
+set -xg fish_user_paths $HOME/node_modules/.bin
+
+set -xg MOKITA_DIR $HOME/Documents/project/mokita-mobile
+set -xg MOKITA_APK_DIR $MOKITA_DIR/android/app/build/outputs/apk
+# set -xg PATH $PATH:$HOME/.yarn/bin
+# set -U fish_user_paths $HOME/.yarn/bin
+
+alias mokita_upload_appdebug="cd $MOKITA_APK_DIR/debug && cp app-debug.apk app-debug-dev-upgrade.apk && gdrive files upload app-debug-dev-upgrade.apk --parent=1bQO7OOXauaS9Rc7HTQFLU7AlJu9c_cOV"
+alias mokita_appdebug="cd $MOKITA_DIR && yarn build-debug && mokita_upload_appdebug && cd $MOKITA_DIR"
+
+set -xg OPENAI_API_KEY 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
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# pnpm
+set -gx PNPM_HOME /Users/aleph/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# php
+
+fish_add_path /usr/local/opt/php@8.0/bin
+fish_add_path /usr/local/opt/php@8.0/sbin
+
+set -gx LDFLAGS "-L/usr/local/opt/php@8.0/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/php@8.0/include"
---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,45 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+wezterm.on("update-right-status", function(window, pane)
+	-- "Wed Mar 3 08:14"
+	local date = wezterm.strftime("%a %b %-d %H:%M ")
+
+	local bat = ""
+	for _, b in ipairs(wezterm.battery_info()) do
+		bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+	end
+
+	window:set_right_status(wezterm.format({
+		{ Text = bat .. "   " .. date },
+	}))
+end)
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- Spawn a fish shell in login mode
+config.default_prog = { "/usr/local/bin/fish", "-l" }
+
+-- For example, changing the color scheme:
+-- config.color_scheme = "AdventureTime"
+config.color_scheme = "Catppuccin Mocha"
+config.tab_bar_at_bottom = false
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.font_size = 14
+config.line_height = 1.0
+
+config.inactive_pane_hsb = {
+	saturation = 0.5,
+	brightness = 0.5,
+}
+
+config.scrollback_lines = 10000
+
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
+
+-- and finally, return the configuration to wezterm
+return config
