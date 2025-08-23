d8c2b365f5995578b7f3fe7e225c8c1a36a435d1
itokun99 <indrawanlisantopersonal@gmail.com>
2025-08-23 21:36:54 +0700

    update config neovim

---
+++ .config/nvim/nvim/lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both"
+o.relativenumber = true
+o.swapfile = false
